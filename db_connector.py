faaa4cbb6672b4e2e973725a382b8c570a967a3b
amodon213 <77975399+amodon213@users.noreply.github.com>
2021-03-22 12:04:05 +0200

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -1,89 +1,88 @@
-from datetime import datetime
-from pathlib import Path
-
-import pymysql
-import json
 import os
+import pymysql
 from dotenv import load_dotenv
+from pathlib import Path
+from datetime import datetime
 
-"""Creds"""
-env_path = Path('.CREDS')
-load_dotenv()
-db_table = os.environ['DB_NAME']
-host_connection = os.environ['SERVER']
-port_connection = os.environ['PORT']
-password_connection = os.environ['DB_PASSWORD']
-username_connection = os.environ['DB_USERNAME']
 creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-connection = pymysql.connect(host='remotemysql.com',
-                             port=3306,
-                             user='cReOSxv10Q',
-                             passwd='iKFLyKI8Aw',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
 
-connection.autocommit(True)
+""" Connection """
+
+env_path = Path('/Users/omerk/PycharmProjects/Python/DevOpsCourse/Projects/Project1') / 'creds'
+load_dotenv(dotenv_path=env_path)
+
+conn = pymysql.connect(host='remotemysql.com',
+                       port=3306,
+                       user='Mmy2cCFQLM',
+                       passwd='KDmN7fJmti',
+                       db='Mmy2cCFQLM',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 
-def add_user(user_id, username):
+conn.autocommit(True)
+
+
+def add_username(user_name, user_id):
     try:
-        connection.connect()
-        cur = connection.cursor()
-        creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        cur.execute(f"INSERT INTO {db_table}.users VALUES (%s, %s, %s)", (user_id, username, creation_date))
+        conn.connect()
+        cur = conn.cursor()
+        cur.execute(f"INSERT INTO {db_table}.users VALUES (%s , %s ,%s )", (user_id, user_name,creation_date))
     except:
-        print("FAILED TO CONNECT")
-    cur.close()
-    connection.close()
+        print('Failed to connect!')
+    finally:
+        cur.close()
+        conn.close()
 
 
-def user_info(user_id):
+def get_user_info(user_id):
     try:
-        connection.connect()
-        cur = connection.cursor()
+        conn.connect()
+        cur = conn.cursor()
         cur.execute(f"SELECT * FROM {db_table}.users WHERE user_id = %s", user_id)
         for row in cur:
             name = row['user_name']
         return name
     except:
-        print("FAILED to get user info!")
+        print('Failed to connect!')
     finally:
         cur.close()
-        connection.close()
+        conn.close()
 
 
-def update_username(user_id, new_name):
+def update_user_name(user_id, n_name):
     try:
-        connection.connect()
-        cur = connection.cursor()
-        cur.execute(f"UPDATE {db_table}.users SET name = %s where id = %s", (new_name, user_id))
+        conn.connect()
+        cur = conn.cursor()
+        cur.execute(f"UPDATE {db_table}.users SET user_name = %s WHERE user_id =  %s", (n_name, user_id))
     except:
-        print("FAILED to modify username!")
+        print('Failed to connect!')
+    finally:
+        cur.close()
+        conn.close()
 
-    cur.close()
-    connection.close()
 
-
-def delete_user_id(user_id):
+def delete_user(user_id):
     try:
-        connection.connect()
-        cur = connection.cursor()
-        cur.execute(f"DELETE FROM {db_table}.users WHERE user_id = %s", user_id)
+        conn.connect()
+        cur = conn.cursor()
+        cur.execute(f"DELETE FROM {db_table}.users WHERE user_id = %s", (user_id))
     except:
-        print("FAILED to DELETE user ID")
-    cur.close()
-    connection.close()
-
+        print('Failed to connect!')
+    finally:
+        cur.close()
+        conn.close()
 
 def get_all_info():
     try:
-        connection.connect()
-        cur = connection.cursor()
-        cur.execute(f'SELECT * FROM {db_table}.users')
+        conn.connect()
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {db_table}.users")
         table = cur.fetchall()
         print(table)
     except:
-        print("FAILED TO CONNECT")
-    finally:
-        cur.close()
-        connection.close()
+        print('Failed to connect!')
+    cur.close()
+    conn.close()
+
+
