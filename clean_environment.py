4580ca04ea0e281c6ec87dcce2ec3f27ab0b5337
amodon213 <77975399+amodon213@users.noreply.github.com>
2021-03-22 12:14:53 +0200

    Update clean_environment.py

--- clean_environment.py
+++ clean_environment.py
@@ -1,4 +1,8 @@
 import requests
 
-requests.get('http://127.0.0.1:5000/stop_server')
-requests.get('http://127.0.0.1:5001/stop_server')
+try:
+    requests.get('http://0.0.0.0:5000/stop_server', timeout=5)
+    print("Backend servers stopped")
+
+except requests.exceptions.ReadTimeout:
+    print("The request timeout -  504 error")
