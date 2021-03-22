ef823a0fa8379275853aa09c63c371b77b164aa6
amodon213 <77975399+amodon213@users.noreply.github.com>
2021-03-22 12:06:25 +0200

    Update backend_testing.py

--- backend_testing.py
+++ backend_testing.py
@@ -1,6 +1,5 @@
 import requests
 from db_connector import *
-sds
 try:
     res = requests.post('http://127.0.0.1:5000/users/8', json={"user_name": "Alex"})  # Adding Alex with User ID 8
     check_user = requests.get('http://127.0.0.1:5000/users/8')  # Step 2 - Checking user ID - 2
